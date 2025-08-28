74d521a64c39ba480127374620b7a79c2abe51e5
JalilAbdullayev <abdullacelil00@gmail.com>
2025-08-28 21:33:03 +0400

    ✨ feat(app): add vercel analytics for tracking user behavior
    - integrate @vercel/analytics to gain insights into user interactions
    - improve understanding of user engagement and optimize app performance

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,11 @@
+<script setup lang="ts">
+import { Analytics } from "@vercel/analytics/nuxt";
+</script>
 <template>
   <UApp>
     <Header />
     <NuxtPage />
     <Footer />
   </UApp>
+  <Analytics />
 </template>
